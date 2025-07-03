b4c23a3cd01f0384b4f542cabaf60039c96457f5
roshinikothapally06 <kothapallyroshini1@gmail.com>
2025-07-03 16:01:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const mod=require('./modules');
+
+console.log(mod.pi);
+console.log(mod.add(10,5));
+console.log(mod.add(10,mod.pi));
---
+++ modules.js
@@ -0,0 +1,5 @@
+const pi=3.14;
+const add=(a,b)=>(a+b);
+
+module.exports.pi=pi;
+module.exports.add=add;
